10c384a32b6eed5d58eb627b6d2d5e3db3742467
Younesfdj <ferradjiyounes69@gmail.com>
2024-05-20 16:21:44 +0100

    modified battery script

--- statusbar/_battery.sh
+++ statusbar/_battery.sh
@@ -7,18 +7,26 @@
 for battery in /sys/class/power_supply/BAT?*; do
 	# If non-first battery, print a space separator.
 	[ -n "${capacity+x}" ] && printf " "
-	# Sets up the status and capacity
-	case "$(cat "$battery/status" 2>&1)" in
-		"Full") status="󰁹" ;;
-		"Discharging") status="󰁽" ;;
-		"Charging") status="󰂄" ;;
-		"Not charging") status="󱃌" ;;
-		"Unknown") status="󰂑" ;;
-		*) exit 1 ;;
-	esac
 	capacity="$(cat "$battery/capacity" 2>&1)"
+	# Sets up the incharge
+	if [ "$(cat "$battery/status" 2>&1)" = "Charging" ]; then
+		incharge="🔌"
+	fi
+	if [ $capacity -le 100 ]; then
+		status="󰁹"
+		if [ $capacity -le 75 ]; then
+			status="󰂀"
+			if [ $capacity -le 50 ]; then
+				status="󰁿"
+				if [ $capacity -le 25 ]; then
+					status="󰁻"
+				fi
+			fi
+		fi
+	fi
+	
 	# Will make a warn variable if discharging and low
-	[ "$status" = "🔋" ] && [ "$capacity" -le 25 ] && warn="❗"
+	[ "$status" = "󰁻" ] && [ "$capacity" -le 25 ] && warn="❗"
 	# Prints the info
-	printf " %s %s%d%%" "$status" "$warn" "$capacity"; unset warn
+	printf " %s%s%s %d%%" "$status" "$warn" "$incharge" "$capacity"; unset warn
 done && printf " | "
