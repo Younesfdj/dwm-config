267d6dcae7ba66918e8601517db9d95ffeb89344
Younesfdj <ferradjiyounes69@gmail.com>
2024-05-21 18:42:17 +0100

    added icons in tags

--- config.def.h
+++ config.def.h
@@ -7,8 +7,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrains Mono:size=11","fontawesome:size=12" };
-static const char dmenufont[]       = "JetBrains Mono:size=11";
+static const char *fonts[]          = { "JetBrains Mono:pixelsize=17","fontawesome:size=12" };
+static const char dmenufont[]       = "JetBrains Mono:pixelsize=17";
 static const char col_gray1[]       = "#282a36";
 static const char col_gray2[]       = "#282a36";
 static const char col_gray3[]       = "#96b5b4";
@@ -26,7 +26,7 @@ static const char *const autostart[] = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "󰨞", "󰈹", "", "", "󰛮", "󰊢", "󰙯","", "󰙎" };
 
 static const Rule rules[] = {
 	/* xprop(1):
